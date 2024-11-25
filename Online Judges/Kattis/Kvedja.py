7b7ecd57b734613109f43cb7da8a22bb91905269
SMH <smh10.mh@gmail.com>
2024-11-25 16:45:08 -0500

    Two Kattis problems' solutions are added.
    Problem Names: "Framtíðar FIFA" and "Kveðja"

---
+++ Online Judges/Kattis/Kvedja.py
@@ -0,0 +1,7 @@
+"""
+https://open.kattis.com/problems/kvedja
+Author: https://github.com/smh997/
+"""
+
+print("Kvedja,")
+print(input())
---
+++ Online Judges/Kattis/fifa.py
@@ -0,0 +1,6 @@
+"""
+https://open.kattis.com/problems/fifa
+Author: https://github.com/smh997/
+"""
+
+print(int(input()) // int(input()) + 2022)
