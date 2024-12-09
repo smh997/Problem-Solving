e84db9346dc46ca906cd26311ca8351a045c497e
SMH <smh10.mh@gmail.com>
2024-12-09 11:51:31 -0500

    Update grading.py

--- Online Judges/Kattis/grading.py
+++ Online Judges/Kattis/grading.py
@@ -1,3 +1,8 @@
+"""
+https://open.kattis.com/problems/grading
+Author: https://github.com/smh997/
+"""
+
 li = list(map(int, input().split())) + [0]
 ans = ['A', 'B', 'C', 'D', 'E', 'F']
 g = int(input())
