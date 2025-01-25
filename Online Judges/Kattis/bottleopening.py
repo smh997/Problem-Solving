c043f1c0571af075496b479dbddc9051bbefb6ff
SMH <smh10.mh@gmail.com>
2025-01-25 18:15:47 -0500

    Update bottleopening.py

--- Online Judges/Kattis/bottleopening.py
+++ Online Judges/Kattis/bottleopening.py
@@ -1,3 +1,8 @@
+"""
+https://open.kattis.com/problems/bottleopening
+Author: https://github.com/smh997/
+"""
+
 n = int(input())
 k = int(input())
 print('impossible' if k >= n else '\n'.join([f'open {i} using {i+1}' for i in range(1, k+1)]))
