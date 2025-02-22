f263fed1337fa42f51b7f81c18d8e7e6ad3a153a
SMH <smh10.mh@gmail.com>
2025-02-22 18:21:33 -0500

    Update laptopsticker.py

--- Online Judges/Kattis/laptopsticker.py
+++ Online Judges/Kattis/laptopsticker.py
@@ -1,2 +1,7 @@
+"""
+https://open.kattis.com/problems/laptopsticker
+Author: https://github.com/smh997/
+"""
+
 wc, hc, ws, hs = map(int, input().split())
 print(1 if wc >= ws + 2 and hc >= hs + 2 else 0)
