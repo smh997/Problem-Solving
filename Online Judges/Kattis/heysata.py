7b5270af1be4dd208a78859ac01ac16cd62ef86b
SMH <smh10.mh@gmail.com>
2025-04-03 13:17:14 -0400

    Kattis solution
    Problem Name: "Heysáta"

---
+++ Online Judges/Kattis/heysata.py
@@ -0,0 +1,7 @@
+"""
+https://open.kattis.com/problems/heysata
+Author: https://github.com/smh997/
+"""
+
+n, ch, inp = int(input()), input(), input()
+print('Unnar fann hana!' if ch in inp else 'Unnar fann hana ekki!')
